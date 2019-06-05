7abead7a7044ed5a9ad84f24007ccebc1934894c
Phil Roth <phil.roth077@gmail.com>
2019-06-05 23:17:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -4,33 +4,33 @@ end
 
 def create_an_array(n1, n2, n3, n4)
   return [n1, n2, n3, n4]
-  
+
 end
 
 def add_element_to_end_of_array(array, element)
-  
+
 end
 
 def add_element_to_start_of_array(array, element)
-  
+
 end
 
 def remove_element_from_end_of_array(array)
-  
+
 end
 
 def remove_element_from_start_of_array(array)
-  
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+
 end
 
 def retrieve_first_element_from_array(array)
-  
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+
 end
